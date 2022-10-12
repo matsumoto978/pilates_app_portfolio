20167d37abd76fdb9ddd22af4eb7949937a4924a
matsumoto978 <myoshi878@gmail.com>
2022-10-13 01:57:06 +0900

    モデルの編集

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,7 @@ class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   has_many :posts, dependent: :destroy
+  has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
 
   has_many :liked_posts, through: :likes, source: :post
