0ebdb21760a34ff05df484ca98d94f056953aa34
matsumoto978 <myoshi878@gmail.com>
2022-10-13 01:18:34 +0900

    likes_count カラムを追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_12_154245) do
+ActiveRecord::Schema.define(version: 2022_10_12_161726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -36,6 +36,7 @@ ActiveRecord::Schema.define(version: 2022_10_12_154245) do
     t.datetime "updated_at", precision: 6, null: false
     t.integer "ward_id", null: false
     t.string "name", null: false
+    t.integer "likes_count", default: 0
     t.index ["user_id"], name: "index_posts_on_user_id"
   end
 
