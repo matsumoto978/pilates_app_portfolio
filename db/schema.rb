88d7f4cf92099b48564274be0a515cb63d82d666
matsumoto978 <myoshi878@gmail.com>
2022-10-14 07:12:10 +0900

    userモデルのnameカラムに制限をつける

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_10_13_220207) do
+ActiveRecord::Schema.define(version: 2022_10_13_220905) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -58,7 +58,7 @@ ActiveRecord::Schema.define(version: 2022_10_13_220207) do
     t.datetime "remember_created_at"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.string "name"
+    t.string "name", null: false
     t.string "img"
     t.text "introduction"
     t.integer "gender", default: 0, null: false
