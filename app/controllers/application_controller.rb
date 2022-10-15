cb75a7ad2dafef89013f022fab73ee4d8b4933b0
matsumoto978 <myoshi878@gmail.com>
2022-10-16 01:34:41 +0900

    usersコントローラーとviewの変更

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,10 @@
 class ApplicationController < ActionController::Base
-  devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :gender, :introduction, :img])
-  devise_parameter_sanitizer.permit(:account_update, keys: [:name, :gender, :introduction, :img])
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name, :gender, :introduction, :img])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name, :gender, :introduction, :img])
+  end
 end
